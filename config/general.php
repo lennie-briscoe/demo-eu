6566c6a90e92e30263eb25e0f1e83ae134736001
Brandon Kelly <brandon@pixelandtonic.com>
2020-01-17 12:16:15 -0800

    Clean up the general settings

--- config/general.php
+++ config/general.php
@@ -10,9 +10,8 @@ return [
 
     // All environments
     '*' => [
-         'useProjectConfigFile' => true,
-
-        // Craft defined config settings
+        'useProjectConfigFile' => true,
+        'allowAdminChanges' => false,
         'defaultSearchTermOptions' => [
             'subLeft' => true,
             'subRight' => true,
@@ -25,8 +24,6 @@ return [
         'maxUploadFileSize' => 20000000,
         'resourceBasePath' => dirname(__DIR__) . '/web/cpresources',
         'maxSlugIncrement' => 100,
-
-        // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
         'aliases' => [
             // @web should be set intentionally
             '@web' => getenv('DEFAULT_SITE_URL'),
@@ -35,52 +32,16 @@ return [
 
     // Production (live) environment
     'production' => [
-        // Disable project config changes on production
-        'allowAdminChanges' => true,
-        'allowUpdates' => false,
-        'backupOnUpdate' => false,
-        'devMode' => false,
-        'enableTemplateCaching' => true,
-        'isSystemLive' => true,
-        // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
-        'aliases' => [
-        ],
-        // Custom site-specific config settings
-        'custom' => [
-        ]
     ],
 
     // Staging (pre-production) environment
     'staging' => [
-        // Disable project config changes on staging
-        'allowAdminChanges' => true,
-        'allowUpdates' => false,
-        'backupOnUpdate' => false,
-        'devMode' => true,
-        'enableTemplateCaching' => true,
-        'isSystemLive' => false,
-        'defaultFileMode' => '0774',
-        // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
-        'aliases' => [
-        ],
-        // Custom site-specific config settings
-        'custom' => [
-        ]
     ],
 
     // Local (development) environment
     'local' => [
-        // Craft defined config settings
-        'allowUpdates' => true,
-        'backupOnUpdate' => true,
+        'allowAdminChanges' => true,
         'devMode' => true,
         'enableTemplateCaching' => false,
-        'isSystemLive' => true,
-        // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
-        'aliases' => [
-        ],
-        // Custom site-specific config settings
-        'custom' => [
-        ]
     ],
 ];
