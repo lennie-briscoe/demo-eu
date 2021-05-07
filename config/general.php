5fa94659ada1deb0b22a5f37d0981ceb649c7717
Matt Stein <matt@pixelandtonic.com>
2021-05-07 09:10:29 -0500

    Once again allow admin changes for demo.

--- config/general.php
+++ config/general.php
@@ -33,7 +33,7 @@ return [
 
     // Production (live) environment
     'production' => [
-        'allowAdminChanges' => false,
+        'allowAdminChanges' => true,
     ],
 
     // Staging (pre-production) environment
