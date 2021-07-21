cbbe4963167c55b09be6b281c8bf27d4dbd76270
Matt Stein <matt@pixelandtonic.com>
2021-07-21 12:34:32 -0500

    No more `postCpLoginRedirect`.

--- config/general.php
+++ config/general.php
@@ -23,7 +23,6 @@ return [
         'securityKey' => App::env('SECURITY_KEY'),
         'maxInvalidLogins' => 1000,
         'maxUploadFileSize' => 20000000,
-        'postCpLoginRedirect' => 'guide/page/welcome',
         'resourceBasePath' => dirname(__DIR__) . '/web/cpresources',
         'maxSlugIncrement' => 100,
         'aliases' => [
